8c3de8041b99147f52aafe47ba7ff8142e9a1995
Samuel Daniel <sanietiedaniel@gmail.com>
2025-03-26 22:42:20 -0400

    solved the missing number challenge

--- src/main/java/com/team8/leetcode/problems/missing_number/MissingNumber.java
+++ src/main/java/com/team8/leetcode/problems/missing_number/MissingNumber.java
@@ -1,5 +1,19 @@
 package com.team8.leetcode.problems.missing_number;
 
+import java.util.Arrays;
+
 public class MissingNumber {
 
+	public int missingNumber(int[] nums) {
+		int n = nums.length;
+		Arrays.sort(nums);
+		
+		for (int i = 0; i < n; i++) {
+			if (nums[i] != i) {
+				return i;
+			}
+
+		}
+		return n;
+	}
 }
