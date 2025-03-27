a8233663cacfcc6d7e82f209e7943731086897d6
Cantona-Bolokang <bolokangcantona@gmail.com>
2025-03-26 21:11:29 -0400

    Solved leetCode problems: AverageSalary and SquaresOfSortedArray.

--- src/main/java/com/team8/leetcode/problems/average_salary/AverageSalary.java
+++ src/main/java/com/team8/leetcode/problems/average_salary/AverageSalary.java
@@ -1,5 +1,36 @@
 package com.team8.leetcode.problems.average_salary;
+import java.util.Arrays;
 
 public class AverageSalary {
+	
+	public double average(int[] salary) {
+        
+		//Initialize sum variable to zero
+		int sum = 0;
+
+
+        //sort the array using Arrays.sort() method
+		//After sorting the array, the minimum salary will be index 0
+		//The maximum salary will be index salary.length - 1
+        Arrays.sort(salary);
+
+
+        //for loop to iterate through the salaries
+        //add the salaries excluding the minimum and the maximum
+        //store the result in the sum variable
+        for(int i = 1; i < salary.length-1; i++){
+            sum+=salary[i];
+        }
+
+        //calculate average salary by dividing the sum by number of salaries excluding minimum and maximum
+        double average = sum/(salary.length-2);
+
+        
+        //return average salary
+        return average;
+
+	}
+
 
 }
+
--- src/main/java/com/team8/leetcode/problems/squares_of_sorted_array/SquaresOfSortedArray.java
+++ src/main/java/com/team8/leetcode/problems/squares_of_sorted_array/SquaresOfSortedArray.java
@@ -1,5 +1,29 @@
 package com.team8.leetcode.problems.squares_of_sorted_array;
+import java.util.Arrays;
 
 public class SquaresOfSortedArray {
+	public int[] sortedSquares(int[] nums) {
+        //initialize arrayLentght variable which is the length of the input array
+        int arrayLength = nums.length;
+        
+       //initialize an empty array that will store the squares
+       //The empty array will have the same size as the input array
+        int [] squares = new int [arrayLength];
+        
+        //Iterate through the input array from index 0 to arrayLength-1
+        //Calculate the square of each value and store it in the squares array
+        for(int i = 0; i < arrayLength; i++){
+            squares[i] = nums[i]*nums[i];
+        }
+        
+        //Use the Arrays.sort() method to sort the array in ascending order
+        Arrays.sort(squares);
+        
+        //return sorted squares array
+        return squares;
+    }
 
 }
+
+
+
